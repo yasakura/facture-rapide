94730e2ad9f1988957a8719bb3514f0521e85b6a
Erwan Leblanc <erwan.leblanc.externe@crossquantum.com>
2017-10-29 11:43:14 +0100

    UPD - lint webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -7,36 +7,36 @@ const babelSettings = JSON.parse(fs.readFileSync('.babelrc')).presets;
 const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
   template: __dirname + '/src/index.html',
   filename: 'index.html',
-  inject: 'body'
+  inject: 'body',
 });
 
 module.exports = {
-    entry: [__dirname + '/src/main.js'],
-    output: {
-        path: path.resolve(__dirname, 'www'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-          {
-            test: /\.(js|jsx)$/,
-            include: [
-              path.resolve(__dirname, "src")
-            ],
-            exclude: [
-              path.resolve(__dirname, "node_modules")
-            ],
-            loader: 'babel-loader',
-            options: {
-              cacheDirectory: true,
-              presets: babelSettings,
-            },
-          }
-      ]
-    },
-    devtool: "source-map",
-    context: __dirname,
-    plugins: [
-      HTMLWebpackPluginConfig
+  entry: [__dirname + '/src/main.js'],
+  output: {
+    path: path.resolve(__dirname, 'www'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: [
+          path.resolve(__dirname, "src"),
+        ],
+        exclude: [
+          path.resolve(__dirname, "node_modules"),
+        ],
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: true,
+          presets: babelSettings,
+        },
+      },
     ],
+  },
+  devtool: "source-map",
+  context: __dirname,
+  plugins: [
+    HTMLWebpackPluginConfig,
+  ],
 };
