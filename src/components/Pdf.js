e18056179cc66ad022ac88e470017d1b355f10a2
Lionel JAMAL <lionel.jamal.externe@crossquantum.com>
2017-11-27 13:42:06 +0100

    UPD - Continue PDF template

--- src/components/Pdf.js
+++ src/components/Pdf.js
@@ -5,25 +5,29 @@ const marginRight = 195;
 const marginTextBottom = 6;
 const colTwo = 142;
 const colThree = 159;
+const footerColOne = 75;
+const footerColTwo = footerColOne;
 
 const Pdf = (props) => {
   let topPosition = 0;
 
   const newLineHeight = (numberMarginTop) => {
-    return topPosition += numberMarginTop;
+    topPosition += numberMarginTop;
+    return topPosition;
   };
 
   const doc = new JsPDF({
     orientation: 'p',
     unit: 'mm',
     format: 'a4',
+    lineHeight: 1.3,
   });
   // Page size A4 : 210 x 297
   doc.setFont('helvetica', 'normal');
   doc.setFontSize(20);
   doc.setTextColor(56, 56, 57);
 
-  newLineHeight(20)
+  newLineHeight(20);
   doc.text(`Code & Smile`, marginLeft, topPosition);
   // doc.text(`Code & Smile ${props.input_1}!`, marginLeft, 28);
   doc.setTextColor('#8a8a8a');
@@ -45,7 +49,7 @@ const Pdf = (props) => {
   doc.text(`Objet : Prestation de développements informatiques`, marginLeft, topPosition);
 
   doc.setLineWidth(0.3);
-  doc.setDrawColor(184,184,184);
+  doc.setDrawColor(184, 184, 184);
   newLineHeight(marginTextBottom);
   doc.line(marginLeft, topPosition, marginRight, topPosition); // x, start y, width, end y
 
@@ -58,7 +62,7 @@ const Pdf = (props) => {
   newLineHeight(marginTextBottom);
   doc.setFont('helvetica', 'normal');
   doc.text(`CROSSQUANTUM`, marginLeft, topPosition);
-  doc.text('Date :', marginRight  - 28, topPosition, null, null, 'right');
+  doc.text('Date :', marginRight - 28, topPosition, null, null, 'right');
   doc.text('26/10/2017', marginRight, topPosition, null, null, 'right');
 
   newLineHeight(marginTextBottom);
@@ -70,7 +74,7 @@ const Pdf = (props) => {
   newLineHeight(marginTextBottom);
   doc.text('N° de TVA Intra : FR71 814770855', marginLeft, topPosition);
 
-  newLineHeight(marginTextBottom *2);
+  newLineHeight(marginTextBottom * 2);
   doc.setDrawColor(56, 56, 57);
   doc.line(marginLeft, topPosition, marginRight, topPosition); // x, start y, width, end y
   doc.setFont('helvetica', 'bold');
@@ -104,6 +108,48 @@ const Pdf = (props) => {
   doc.setFont('helvetica', 'bold');
   doc.text('Total TTC à payer', colThree, topPosition, null, null, 'right');
   doc.text('10,494.00 €', marginRight, topPosition, null, null, 'right');
+
+  doc.setDrawColor(184, 184, 184);
+  newLineHeight(marginTextBottom);
+  doc.line(marginLeft, topPosition, marginRight, topPosition); // x, start y, width, end y
+
+  newLineHeight(marginTextBottom);
+  doc.setFont('helvetica', 'normal');
+  doc.setFontSize(10);
+  doc.text('Date limite de paiement : ', footerColOne, topPosition, null, null, 'right');
+  doc.text('15/10/2017', footerColTwo, topPosition);
+
+  newLineHeight(marginTextBottom);
+  doc.text('Mode de paiement : ', footerColOne, topPosition, null, null, 'right');
+  doc.text('- si nécessaire, par chèque à l\'ordre de Code & Smile', footerColTwo, topPosition);
+
+  newLineHeight(marginTextBottom);
+  doc.text('- de préférence, par virement sur le compte bancaire suivant :', footerColTwo, topPosition);
+
+  newLineHeight(marginTextBottom);
+  doc.setDrawColor(56, 56, 57);
+  doc.line(marginLeft, topPosition, marginRight, topPosition); // x, start y, width, end y
+  doc.line(marginLeft, topPosition, marginLeft, topPosition + 22);
+  doc.line(marginRight, topPosition, marginRight, topPosition + 22);
+
+  newLineHeight(marginTextBottom);
+  doc.text('Banque  : ', footerColOne, topPosition, null, null, 'right');
+  doc.text('BNPPARB PLESSIS ROBINSON', footerColTwo, topPosition);
+
+  newLineHeight(marginTextBottom);
+  doc.text('IBAN  : ', footerColOne, topPosition, null, null, 'right');
+  doc.text('FR76 3000 4002 8100 0100 9113 805', footerColTwo, topPosition);
+
+  newLineHeight(marginTextBottom);
+  doc.text('BIC  : ', footerColOne, topPosition, null, null, 'right');
+  doc.text('BNPA FR PP XXX', footerColTwo, topPosition);
+
+  doc.line(marginLeft, topPosition + 4, marginRight, topPosition + 4); // x, start y, width, end y
+
+  newLineHeight(marginTextBottom * 1.7);
+  const toto = 'Aucun escompte pour règlement anticipé.\nEn application de la loi n°92-1442 du 31.12.1992, les factures sont payables à l\'échéance indiquée.\nTout règlement effectué après expiration de ce délai donnera lieu, à titre de pénalité de retard, à l’application d’un intérêt égal à celui appliqué par la Banque Centrale Européenne à son opération de refinancement la plus récente, majoré de 10 points de pourcentage, ainsi qu\'une indemnité forfaitaire de 40 € pour frais de recouvrement.';
+  const textLines = doc.setFont('helvetica', 'normal').setFontSize(6.5).splitTextToSize(toto, 180);
+  doc.text(textLines, marginLeft, topPosition);
 
   if (window.cordova) {
     // For save iPhone :
