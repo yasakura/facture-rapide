2e8d6912bc2d6c9b3da6ca6eca38d7bd86dcd441
Lionel JAMAL <freelance@ljamal.net>
2018-01-20 14:12:03 +0100

    Layout iPhone X

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'framework7/dist/css/framework7.css';
 import 'framework7/dist/css/framework7.ios.min.css';
 import 'framework7/dist/css/framework7.ios.colors.min.css';
+import './assets/css/custom.css';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
